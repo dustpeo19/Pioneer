9ceb6e67d14eaa422408975ceaf1e8b83cd15682
dustpeo19 <sm18570@naver.com>
2020-12-04 03:23:33 +0900

    pioneer3
    Julia added

---
+++ js/header-shrink.js
@@ -0,0 +1,13 @@
+var $window=$(window), $mainheader=$('#main-header');
+
+$window.scroll(function(){
+    if($(this).scrollTop()>200){
+        if(!$mainheader.hasClass('shrink')){
+            $mainheader.addClass('shrink');
+        }
+    }else{
+        if($mainheader.hasClass('shrink')){
+            $mainheader.removeClass('shrink');
+        }
+    }
+});
